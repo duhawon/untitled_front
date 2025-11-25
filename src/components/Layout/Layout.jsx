87d1e932272b3cdc3cf683445d2d0130363937ad
duhawon <duhawon1123@gmail.com>
2025-11-26 00:31:26 +0900

    Layout main에 여백 추가

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
+import './Layout.css';
+
 const Layout = (props) => {
     return (
-        <div>
-            <Header/>
-            <main>
+        <div className="layout-wrapper">
+            <Header />
+            <main className="layout-content">
                 {props.children}
             </main>
-            <Footer/>
+            <Footer />
         </div>
     );
 }
