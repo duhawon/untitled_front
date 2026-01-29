70e74ac1d9fbd45636cfae68287dbca9f0a8515c
duhawon <duhawon1123@gmail.com>
2026-01-30 00:15:37 +0900

    BrowserRouter 위치조정 - 로그아웃 시 profile page 사용자 정보 null 오류

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,25 +1,26 @@
 import React from "react";
 import Main from './pages/Main';
 import RoomDetail from './pages/RoomDetail';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import ReviewList from './pages/ReviewList';
 import ReviewDetail from './pages/ReviewDetail';
 import SearchResultPage from './pages/SearchResultPage';
 import Profile from './pages/Profile';
+import ProtectedRoute from './routes/ProtectedRoute';
 
 function AppRouter() {
     return (
         <div>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Main />} />
-                    <Route path="room/:roomId" element={<RoomDetail />} />
-                    <Route path="room/:roomId/reviews" element={<ReviewList />} />
-                    <Route path="review/:reviewId" element={<ReviewDetail />} />
-                    <Route path="search/:keyword" element={<SearchResultPage />} />
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="room/:roomId" element={<RoomDetail />} />
+                <Route path="room/:roomId/reviews" element={<ReviewList />} />
+                <Route path="review/:reviewId" element={<ReviewDetail />} />
+                <Route path="search/:keyword" element={<SearchResultPage />} />
+                <Route element={<ProtectedRoute />}>
                     <Route path="profile/:userId" element={<Profile/>} />
-                </Routes>
-            </BrowserRouter>
+                </Route>
+            </Routes>
         </div>
     );
 };
